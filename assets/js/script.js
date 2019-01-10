56b8eaee2481941207cde7cf42b69f4028693ef5
Vladlena Costescu <vladimir.costescu@gmail.com>
2019-01-10 06:20:41 -0500

    move JS event handler to separate file as function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 // enable the ClipboardJS plugin to bind events to buttons
 new ClipboardJS('.a-btn');
+
+function onClickHandler(heading_id) {
+    if (!heading_id) {
+        return;
+    }
+
+    var selector = '#' + heading_id + '-container';
+    $(selector).toggle();
+}
